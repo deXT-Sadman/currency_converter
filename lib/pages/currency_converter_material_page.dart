3df9f9e0ac091bcfc0cfe469cdc7019b53cbd418
Sadman <2258cse00945@manarat.ac.bd>
2026-05-03 02:59:03 +0600

    fix: Ensure proper disposal of TextEditingController in CurrencyConverterMaterialPage

--- lib/pages/currency_converter_material_page.dart
+++ lib/pages/currency_converter_material_page.dart
@@ -20,6 +20,12 @@ class _CurrencyConverterMaterialPageState
   }
 
   @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final border = OutlineInputBorder(
       borderRadius: BorderRadius.circular(10),
